a4f714f86c3e50f77eabaced08f576f7e6268b64
vishal patidar <vishalpatidar963027@gmail.com>
2021-02-17 10:01:11 +0530

    dealing with null and invalid stock symbole

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,7 +1,17 @@
 class StocksController < ApplicationController
   def search
     # byebug
-    @stock = Stock.new_lookup(params[:stock])
-    render 'users/my_portfolio'
+    if params[:stock].present?
+      @stock = Stock.new_lookup(params[:stock])
+      if @stock
+        render 'users/my_portfolio'
+      else
+        flash[:alert] = "please enter a valid symbole to search"
+        redirect_to my_portfolio_path
+      end
+    else
+      flash[:alert] = "please enter a symbole to search"
+      redirect_to my_portfolio_path
+    end
   end
 end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -3,8 +3,11 @@ class Stock < ApplicationRecord
     client = IEX::Api::Client.new(
        publishable_token: Rails.application.credentials.iex_client[:publishable_token_api_key],
        secret_token: Rails.application.credentials.iex_client[:secret_token_api_key],
-       endpoint: 'https://sandbox.iexapis.com/v1'
-    )
-    new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol)) #it will be return stock price
+       endpoint: 'https://sandbox.iexapis.com/v1')
+    begin
+      new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol)) #it will be return stock price
+    rescue => exception
+      return nil
+    end
   end
 end

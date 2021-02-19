624a123a2d1db78e3ffdca820d00363955857780
vishal patidar <vishalpatidar963027@gmail.com>
2021-02-19 18:18:51 +0530

    add user_stock association controller and model and change in view

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,9 @@
 class Stock < ApplicationRecord
+  has_many :user_stocks
+  has_many :users, through: :user_stocks
+
+  validates :name, :ticker ,presence: true
+
   def self.new_lookup(ticker_symbol) #it is a class method
     client = IEX::Api::Client.new(
        publishable_token: Rails.application.credentials.iex_client[:publishable_token_api_key],
@@ -9,5 +14,9 @@ class Stock < ApplicationRecord
     rescue => exception
       return nil
     end
+  end
+
+  def self.check_db(ticker_symbol)
+    where(ticker: ticker_symbol).first
   end
 end

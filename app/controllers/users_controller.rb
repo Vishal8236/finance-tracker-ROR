4e877e114927fd60bc4d27b2780a7c697162fe41
vishal patidar <vishalpatidar963027@gmail.com>
2021-02-20 17:55:55 +0530

    Add friendships self-referencial association from back-end

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,8 @@ class UsersController < ApplicationController
   def my_portfolio
     @tracked_stocks = current_user.stocks
   end
+
+  def my_friends
+    @myFriend = current_user.friends
+  end
 end
